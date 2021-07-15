163fa53aa6c32107761a92b3856fba36137474cc
José Arvizu <joseantoniod63@gmail.com>
2021-07-15 12:33:29 -0500

    Update CameraRead.py

--- digitRecognition/CameraRead.py
+++ digitRecognition/CameraRead.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score, precision_score, f1_score
 from sklearn.svm import SVC
 
-df = pd.read_csv('/Users/arvizu/Downloads/UP/SEASON6/MachineLearning/Python/digitRecognition/camera.csv',sep=",")
+df = pd.read_csv('camera.csv',sep=",")
 X = df.values[0:10000,1:]
 Y = df.values[:,0]
 
@@ -24,16 +24,11 @@ model.fit(Xtrain,ytrain)
 ypred = model.predict(Xtest)
 print(Xtrain.shape, Xtest.shape, len(ytrain), len(ytest))
 print('accuracy_score',accuracy_score(ytest,ypred))
-print('Número de muestras:',len(ytest))
-print('Número de predicciones correctas:', np.sum(ytest==ypred))
+print('Samples:',len(ytest))
+print('Correct predictions:', np.sum(ytest==ypred))
 print('macro-precision:',precision_score(ytest,ypred,average='macro'))
 print('macro-recall:',recall_score(ytest,ypred,average='macro'))
 print('macro-f1:',f1_score(ytest,ypred,average='macro'))
-###MODELO################
-
-
-
-######BILL RECOGNITION#########
 
 
 def function(img):
@@ -47,27 +42,15 @@ def function(img):
     for rect in rectangles:
         datasetTestCamera = []
         if (rect[2] > 50 and rect[3]>50) and (rect[2] < 600 and rect[3]<600): 
-            #print('img_shape',img.shape)
             imgn = img[ rect[1]:rect[1]+rect[3] , rect[0]:rect[0]+rect[2]]
-            #cv2.imshow("Imagen nueva", imgn)
-            #cv2.imwrite('CameraImages/img'+str(cont)+'.png',imgn)
-            #imgn = cv2.resize(imgn,(100,100))
-            #cv2.imwrite('CameraImages/img'+str(cont)+'.png',imgn)
             cont+=1
-
-            ##################OBTENER HISTOGRAMA##################
             img_hsv = cv2.cvtColor(imgn, cv2.COLOR_BGR2HSV)
 
-            #print('img',img.shape)
             
             lower = np.array([0,0,0])
             upper = np.array([179,255,127])
-
-            #Encontrar los pixeles de la imagen azules
             mask = cv2.inRange(img_hsv, lower, upper) 
             
-            #print('img_hsv',img_hsv.shape)
-            #nrows, ncols, nch = img_hsv.shape
             mask = cv2.resize(mask, (28, 28), interpolation=cv2.INTER_AREA)
             cv2.imshow("Imagen nueva", mask)   
             # Vectorizar la imagen
@@ -85,27 +68,18 @@ def function(img):
 	                X.append(k)
             
             
-            #print('Ximg_hsv',Ximg_hsv.shape)
 
             # Hue | Value | Saturation
             # 24  |  92   |  156
             # 100 |  234  |  234
 
-            # Obtener hue
-            #X = X.reshape(1,-1)
             
             ypred = model.predict([X])
             print("-----------Imagen camara: ",ypred,"-----------")
             print(X)
-            #print("##Predicción del Modelo##", ypred)
-            ##################OBTENER HISTOGRAMA##################
-
-            cv2.imwrite('CameraImages/img'+str(cont)+'.png',mask)
-            
+            cv2.imwrite('CameraImages/img'+str(cont)+'.png',mask)            
 
             # Clasificar la imagen
-
-
             cv2.rectangle(img, (rect[0],rect[1]),(rect[0]+rect[2],rect[1]+rect[3]), (0,255,0),2)
             cv2.putText(img, str(ypred), (rect[0], rect[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 0), 3, cv2.LINE_AA)
  
